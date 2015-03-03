b9d29015d5eeed7bca23165ef8df3446c387fc40
tejaswiniG <tejaswini_grandhe@persistent.co.in>
2015-03-03 13:59:12 +0530

    Update new_file.py

--- new_file.py
+++ new_file.py
@@ -1,2 +1,4 @@
 print "This is the sample file for testing"
 print "-----This is EOF----"
+#this line is added from github - this shud get reflected while pulling the data in local repo
+print "This file is pushed from linux console"
